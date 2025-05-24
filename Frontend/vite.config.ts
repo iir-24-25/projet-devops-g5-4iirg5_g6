f062ae28c74ec17eed9a2cc163aabe9606b96089
mouadza <moaad.za12@gmail.com>
2025-05-24 10:48:41 +0100

    Frontend Integration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,10 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      crypto: 'crypto-browserify'
+      crypto: 'crypto-browserify' // Polyfill crypto
     }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  }
 });
