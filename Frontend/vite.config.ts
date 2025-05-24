eb3e46c69611e927914b16cf219ee6a15f33e1ed
mouadza <moaad.za12@gmail.com>
2025-05-24 02:13:09 +0100

    Frontend Integration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify'
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
